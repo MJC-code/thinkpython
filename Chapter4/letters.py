aaf6407d5c408979992ca9bc2c48816d5177452a
Mark <cookson.mark@gmail.com>
2021-10-18 17:03:10 +1300

    continued letters.py

--- Chapter4/letters.py
+++ Chapter4/letters.py
@@ -1,44 +1,88 @@
 """Draws letters with scale n"""
 
-
 import turtle
+import polygon
+import math
 
-# The following definitions are needed to use the typewriter.py function given by the author
+def fdbk(t, n):
+    t.fd(n)
+    t.bk(n)
 
-def fd(t, length):
-    t.fd(length)
-
-def bk(t, length):
-    t.bk(length)
-
-def lt(t, angle=90):
-    t.lt(angle)
-
-def rt(t, angle=90):
-    t.rt(angle)
-
-def pd(t):
-    t.pd()
-
-def pu(t):
-    t.pu()
-
-def skip(t, n):
-    """lift the pen and move"""
-    pu(t)
-    fd(t, n)
-    pd(t)
-
-
-
-
-
-
+def add_space(t, n):
+        t.pu()
+        t.setheading(0)
+        t.fd(n / 3)
+        t.pd()
+        
 def draw_a(t, n):
     t.setheading(75)
-    t.fd(n*3)
+    t.fd(2*n / math.sin (math.radians(75)))
     t.setheading(-75)
-    t.fd(n*3)
-    t.setheading(0)
+    t.fd(2*n / math.sin (math.radians(75)))
+    t.lt(180)
+    t.fd(n / math.sin (math.radians(75)))
+    t.setheading(180)
+    fdbk(t, n/2)
+    t.setheading(-75)
+    t.fd(n / math.sin (math.radians(75)))
+    add_space(t, n)
     
+def draw_b(t, n):
+    t.setheading(0)
+    polygon.arc(bob, n/2, 180)
+    t.setheading(0)
+    polygon.arc(bob, n/2, 180)
+    t.setheading(270)
+    t.fd(2 * n)
+    t.setheading(0)
+    t.pu()
+    t.fd(n)
+    add_space(t, n)
+
+def draw_c(t, n):
+    t.pu()
+    t.setheading(90)
+    t.fd(n * 2)
+    t.rt(90)
+    t.fd(n/3)
+    t.setheading(180)
+    t.pd()
+    polygon.arc(bob, n, 180)
+    add_space(t, n)
+    
+def draw_d(t, n):
+    t.setheading(0)
+    polygon.arc(bob, n, 180)
+    t.setheading(270)
+    t.fd(n*2)
+    add_space(t, 4*n)
+
+def draw_e(t, n):
+    t.setheading(90)
+    t.fd(2 * n)
+    t.rt(90)
+    fdbk(t, n)
+    t.setheading(270)
+    t.fd(n)
+    t.lt(90)
+    fdbk(t, n)
+    t.setheading(270)
+    t.fd(n)
+    t.lt(90)
+    
+    t.fd(n)
+    add_space(t, n)
+    
+    
+scale = 30
+bob = turtle.Turtle()
+draw_a(bob, scale)
+
+draw_b(bob, scale)
+draw_c(bob, scale)
+draw_d(bob, scale)
+draw_e(bob, scale)
+turtle.mainloop()
+
+
     
