79107b96d70f69fd9d8c4995ee2e3d1370c0ee83
Mark C <cookson.mark@gmail.com>
2021-11-03 19:44:33 +1300

    Started revised version of letters.py

--- Chapter4/letters.py
+++ Chapter4/letters.py
@@ -1,91 +1,141 @@
-"""Draws letters with scale n"""
+"""Draws letters with scale n - most letters are 2n high and 1n wide"""
 
 import turtle
 import polygon
 import math
 
+
+""" All definitions below start and end in bottom left corner of letter
+"""
+
 def fdbk(t, n):
-    t.fd(n)
-    t.bk(n)
-
-def add_space(t, n):
-        t.pu()
-        t.setheading(0)
-        t.fd(n / 3)
-        t.pd()
-        
-def draw_a(t, n):
-    t.setheading(75)
-    t.fd(2*n / math.sin (math.radians(75)))
-    t.setheading(-75)
-    t.fd(2*n / math.sin (math.radians(75)))
-    t.lt(180)
-    t.fd(n / math.sin (math.radians(75)))
-    t.setheading(180)
-    fdbk(t, n/2)
-    t.setheading(-75)
-    t.fd(n / math.sin (math.radians(75)))
-    add_space(t, n)
-    
-def draw_b(t, n):
-    t.setheading(0)
-    polygon.arc(bob, n/2, 180)
-    t.setheading(0)
-    polygon.arc(bob, n/2, 180)
-    t.setheading(270)
-    t.fd(2 * n)
-    t.setheading(0)
-    t.pu()
-    t.fd(n)
-    add_space(t, n)
-
-def draw_c(t, n):
-    t.pu()
-    t.setheading(90)
-    t.fd(n * 2)
-    t.rt(90)
-    t.fd(n/3)
-    t.setheading(180)
     t.pd()
-    polygon.arc(bob, n, 180)
-    add_space(t, n)
-    
-def draw_d(t, n):
-    t.setheading(0)
-    t.pu
-    
+    t.fd(n)
+    t.lt(180)
+    t.fd(n)
+    t.lt(180)
 
-    polygon.arc(bob, n, 180)
-    t.setheading(270)
-    t.fd(n*2)
-    add_space(t, 4*n)
 
-def draw_e(t, n):
+def left_bottom(t, n):
+    t.pd()
     t.setheading(90)
-    t.fd(2 * n)
-    t.rt(90)
     fdbk(t, n)
-    t.setheading(270)
-    t.fd(n)
-    t.lt(90)
-    fdbk(t, n)
-    t.setheading(270)
-    t.fd(n)
-    t.lt(90)
-    
-    t.fd(n)
-    add_space(t, n)
+    t.setheading(0)
 
-    
+def left_top(t, n):
+    t.setheading(90)
+    t.pu()
+    t.fd(n)
+    t.pd()
+    fdbk(t,n)
+    t.pu()
+    t.setheading(270)
+    t.fd(n)
+    t.setheading(0)
+
+def right_bottom(t, n):
+    t.setheading(0)
+    t.pu()
+    t.fd(n)
+    t.pd()
+    left_bottom(t,n)
+    t.setheading(180)
+    t.pu()
+    t.fd(n)
+    t.setheading(0)
+
+def right_top(t, n):
+    t.setheading(0)
+    t.pu()
+    t.fd(n)
+    t.pd()
+    left_top(t,n)
+    t.setheading(180)
+    t.pu()
+    t.fd(n)
+    t.setheading(0)
+
+def bottom_beam(t, n):
+    t.setheading(0)
+    t.pd()
+    fdbk(t, n)
+
+def mid_beam(t,n):
+    t.setheading(90)
+    t.pu()
+    t.fd(n)
+    bottom_beam(t, n)
+    t.setheading(270)
+    t.pu()
+    t.fd(n)
+    t.setheading(0)
+
+def top_beam(t,n):
+    t.setheading(90)
+    t.pu()
+    t.fd(2*n)
+    bottom_beam(t, n)
+    t.setheading(270)
+    t.pu()
+    t.fd(2*n)
+    t.setheading(0)
+
+def space(t, n):
+    t.setheading(0)
+    t.pu()
+    t.fd(2 * n)
+    t.pd()
+
+""" Individual letter definitions"""
+def draw_e(t, n):
+    bottom_beam(t, n)
+    mid_beam(t, n)
+    top_beam(t, n)
+    left_bottom(t, n)
+    left_top(t, n)
+    space(t, n)
+
+def draw_f(t, n):
+    mid_beam(t, n)
+    top_beam(t, n)
+    left_bottom(t, n)
+    left_top(t, n)
+    space(t, n)
+
+def draw_h(t, n):
+    mid_beam(t, n)
+    left_bottom(t, n)
+    left_top(t, n)
+    right_bottom(t, n)
+    right_top(t, n)
+    space(t, n)
+
+def draw_i(t, n):
+    bottom_beam(t, n)
+    top_beam(t, n)
+    t.fd(n / 2)
+    left_bottom(t, n)
+    left_top(t, n)
+    space(t, n * 0.75)
+
+
+def draw_l(t, n):
+    bottom_beam(t, n)
+    left_bottom(t, n)
+    left_top(t, n)
+    space (t, n)
     
 scale = 30
 bob = turtle.Turtle()
-draw_a(bob, scale)
-
-draw_b(bob, scale)
-draw_c(bob, scale)
-draw_d(bob, scale)
-draw_e(bob, scale)
+#left_bottom(bob, scale)
+#left_top(bob, scale)
+#right_bottom(bob, scale)
+#right_top(bob, scale)
+#draw_e(bob, scale)
+#draw_f(bob, scale)
+#draw_h(bob, scale)
+#draw_i(bob, scale)
+draw_l(bob, scale)
 turtle.mainloop()
 
 
