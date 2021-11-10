2016fd9fcc59a375e284ac0595a9a6cb217e964c
Mark C <cookson.mark@gmail.com>
2021-11-10 15:00:29 +1300

    Finished ex 5.2

--- Chapter5/ex5.2.py
+++ Chapter5/ex5.2.py
@@ -3,13 +3,29 @@
 def check_fermat(a, b, c, n):
     """Checks whether 4 positive integers obey Fermat's last theorem
     (a**n + b**n = c**n doesn't hold for n>2)"""
-    
     for i in [a, b, c, n]:
-        if type(i) != int or i < 1:
-            print('All inputs must be positive integers')
+        if i < 1:
+            print('This function only accepts positive integers')
             return
+        
     if a**n + b**n == c **n and n>2:
-        print ('Holy smokes, Fermat was wrong!')
+        print ('\nHoly smokes, Fermat was wrong!')
     else:
-        print ("No that doesn't work.")
+        print ("\nNo that doesn't work.")
+
+
+def input_fermat():
+    """ Asks the user to imput 4 values, converts them to integers, then uses
+    check_fermat to check whether they violate Fermat's theorem"""
+    
+    print("You will be asked to input 4 values. They will then be tested to check\n\
+whether they violate Pascal's Last Theorem (There are no positive integers\n\
+such that a**n + b**n = c**n for n>2")
+    
+    a = int(input('Enter a positive integer a:\n'))
+    b = int(input('Enter a positive integer b:\n'))
+    c = int(input('Enter a positive integer c:\n'))
+    n = int(input('Enter a positive integer n:\n'))
+    
+    check_fermat(a, b, c, n)
 
