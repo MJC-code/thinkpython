2ff77fad3c6766f9012e8c6a5b7d624239258799
Mark <cookson.mark@gmail.com>
2021-11-22 18:14:45 +1300

    Started Chapter 7 exercises

---
+++ Chapter7/ex7_1.py
@@ -0,0 +1,29 @@
+import math
+
+def mysqrt(a):
+     epsilon = 0.0000001
+     x = a / 2
+
+     while True:
+         y = (x + a/x) / 2
+         if abs (y-x) < epsilon:
+             break
+         x = y
+    
+     return y
+
+
+
+def test_square_root(maximum):
+    i = 1
+    print ('{:<5}'.format('a'), '{:<25}'.format('mysqrt(a)'), '{:<25}'.format('math.sqrt(a)'), '{:<25}'.format('diff'))
+    print ('{:<5}'.format('-'), '{:<25}'.format('---------'), '{:<25}'.format('------------'), '{:<25}'.format('----'))
+
+    while (i <= maximum):
+        print ('{:<5}'.format(i), '{:<25}'.format(mysqrt(i)), '{:<25}'.format(math.sqrt(i)), '{:<25}'.format(abs(mysqrt(i) - math.sqrt(i))))
+        i += 1
+        
+            
+       
+    
+test_square_root(10)
