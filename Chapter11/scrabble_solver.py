faa56ce829bf94af8b1bde525cce06ec3bfb5d5a
Mark <cookson.mark@gmail.com>
2021-12-23 15:37:54 +1300

    Made crossword_solver.py work

--- Chapter11/scrabble_solver.py
+++ Chapter11/scrabble_solver.py
@@ -1,22 +1,29 @@
 def scrabble_solver(rack_letters):
+     """Takes a string rack_letters, returns a list of words that can be formed
+using those letters"""
      word_list = {}
      result = []
-     rack_letters = histogram(rack_letters)
+     histogram_rack_letters = histogram(rack_letters)
+     
      fin = open('words.txt')   
      for word in fin:
-          word_list[word] = histogram(word.strip())
-
-#     for i in word_list:
-#          print(i, word_list[i])
-     for word in word_list:
-          for letter in word_list[word]:
-               print(word[letter])
+          word_list[word] = (histogram(word.strip()))
           
-         
-                
-
-
+     for word, histogram_word_letters in word_list.items():
+          if can_be_formed(histogram_word_letters, histogram_rack_letters):
+               result.append(word.strip())
      return result
+
+
+
+
+def can_be_formed(histogram_word_letters, histogram_rack_letters):
+     """Takes two dictionaries of letter frequencies,
+returns True if histogram_word can be formed with letters of histogram_letters"""
+     for item in histogram_word_letters:
+          if histogram_word_letters[item] > histogram_rack_letters.get(item, 0):
+               return False
+     return True
           
           
 
@@ -30,5 +37,4 @@ def histogram(s):
                d[c] += 1
      return d
 
-print(scrabble_solver('aa'))
-
+print(scrabble_solver('laura'))
