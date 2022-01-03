43e5ce2bf8de289c45103af291a87b6bfa243ef3
Mark C <cookson.mark@gmail.com>
2022-01-03 16:52:36 +1300

    Found better solution to Ex 10-4

--- Chapter12/Ex12-4.py
+++ Chapter12/Ex12-4.py
@@ -1,6 +1,4 @@
 reducible_dict = dict()
-#reducible_dict['a'] = True
-#reducible_dict['i'] = True
 reducible_dict[''] = True
 
 
@@ -17,10 +15,8 @@ def word_dict_setup(filename):
 
 def find_children(word, word_dict):
     res = []
-   
     for index in range(len(word)):
         new_word = word[:index] + word[index+1:]
-
         if new_word in word_dict:
             res.append(new_word)          
         index += 1
@@ -36,32 +32,45 @@ def is_reducible(word, word_dict):
             return False
     else:
         for i in find_children(word, word_dict):
-            return is_reducible(i, word_dict)
+            reducible_dict[i] = is_reducible(i, word_dict)
+            return reducible_dict[i]
+
     reducible_dict[word] = False
     return False
+
         
 def search_for_reducible_words(word_dict):
-    longest = 1
+    result = {}
     for word in word_dict:
-        if len(word) < longest:
-            continue
         if is_reducible(word, word_dict):
             reducible_dict[word] = True
-            if len(word) >= longest:
-                print(word)
-                longest = len(word)
+            result[word] = (len(word), find_reducible_chain(word, word_dict))
+    return result
             
-            
+def find_reducible_chain(word, word_dict):
+    result = ''
+    for child in find_children(word, word_dict):
+
+        if is_reducible(child, word_dict):
+            result = result + child + '-->' + find_reducible_chain(child, word_dict)
+    return result
+
+
+def find_longest_reducible_words(word_dict):
+    d = search_for_reducible_words(word_dict)
+    result = []
+    for word in sorted(d, key=d.get, reverse=True):
+        result.append((word, d[word]))
         
+
+    return result[0:5]
     
-#find_reducible_words('words.txt')
+
 word_dict = word_dict_setup('words.txt')
-#for i in word_dict:
-#    reducible_dict[i] = None
-#for i in t:
-#    print (i)
-#print (find_children('sprite', word_dict))
-#print(is_reducible('please', word_dict))
-search_for_reducible_words(word_dict)
+
+t = find_longest_reducible_words(word_dict)
+for i in t:
+    print(i, '\n')
+
 
                            
