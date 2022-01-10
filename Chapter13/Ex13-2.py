e30c5eb1cff0eecc46286a9603fbf1a3cb9a4b0e
Mark <cookson.mark@gmail.com>
2022-01-10 22:49:06 +1300

    Finished Ex 13-3

--- Chapter13/Ex13-2.py
+++ Chapter13/Ex13-2.py
@@ -5,7 +5,7 @@ def process_line(line):
     line = line.replace('-', ' ' )
     line = line.replace('—', ' ' )
     for word in line.split():
-        word = (word.strip(string.whitespace + string.punctuation + '“' + '”'))
+        word = (word.strip(string.whitespace + string.punctuation + '“' + '”' + '‘' + '’'))
         word = word.lower()
         if word not in word_usage:
             word_usage[word] = 1
@@ -18,9 +18,9 @@ def remove_header(gutenberg_file):
         if '*** START OF' in line:
             break
 
-def analyse_book(gutenberg_file):
+def analyse_book(gutenberg_file, title):
     """Takes a text file book from Project Gutenberg,
-    returns a dictionary of word usage -> frequency"""
+    returns the number of unique words in the book"""
     word_usage.clear()
     fin = open(gutenberg_file)
     remove_header(fin)
@@ -28,13 +28,22 @@ def analyse_book(gutenberg_file):
         if '*** END OF' in line:
             break
         process_line(line.strip())
-    return word_usage
+    return len(word_usage)
 
 
 # This text uses open and close quotes which are not part of string.punctuation
-d = analyse_book('pride_and_prejudice.txt')
-count = 1
-for i in d:
-    print(count, i)
-    count += 1
+d = analyse_book('pride_and_prejudice.txt', 'Pride and Prejudice')
+print('Pride and Prejudice uses', d, 'unique words.')
+
+d = analyse_book('gatsby.txt', 'The Great Gatsby')
+print('The Great Gatsby uses', d, 'unique words.')
+
+d = analyse_book('defoe.txt', 'Robinson Crusoe')
+print('Robinson Crusoe uses', d, 'unique words.')
+
+d = analyse_book('maugham.txt', 'Of Human Bondage')
+print('Of Human Bondage uses', d, 'unique words.')
+
+
+
 
--- Chapter13/Ex13-3.py
+++ Chapter13/Ex13-3.py
@@ -1,20 +1,53 @@
 import string
+word_usage = dict()
 
 def process_line(line):
-    result = []    
+    line = line.replace('-', ' ' )
+    line = line.replace('—', ' ' )
     for word in line.split():
-        word = (word.strip(string.whitespace + string.punctuation + '“' + '”'))
+        word = (word.strip(string.whitespace + string.punctuation + '“' + '”' + '‘' + '’'))
         word = word.lower()
-        print(word)
+        if word not in word_usage:
+            word_usage[word] = 1
+        else:
+            word_usage[word] += 1
     
+            
 def remove_header(gutenberg_file):
     for line in gutenberg_file:
         if '*** START OF' in line:
             break
 
+def analyse_book(gutenberg_file):
+    """Takes a text file book from Project Gutenberg,
+    returns the number of unique words in the book"""
+    result = []
+    word_usage.clear()
+    fin = open(gutenberg_file)
+    remove_header(fin)
+    for line in fin:
+        if '*** END OF' in line:
+            break
+        process_line(line.strip())
+    for word in sorted(word_usage, key=word_usage.get, reverse = True):
+        result.append(word)
+    return result[0:20]
 
-fin = open('jane_austen.txt')
+
 # This text uses open and close quotes which are not part of string.punctuation
-remove_header(fin)
-for line in fin:
-    process_line(line.strip())
+d = analyse_book('pride_and_prejudice.txt')
+print('Pride and Prejudice top 20 words are:', d, '\n')
+
+
+d = analyse_book('gatsby.txt')
+print('The Great Gatsby top 20 words are:', d, '\n')
+
+d = analyse_book('defoe.txt')
+print('Robinson Crusoe top 20 words are:' , d, '\n')
+
+d = analyse_book('maugham.txt')
+print('Of Human Bondage top 20 words are:', d, '\n')
+
+
+
+
