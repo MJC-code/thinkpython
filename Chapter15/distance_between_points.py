0153751d0795f0ffaceed4d1061b4a1352b5de4e
Mark <cookson.mark@gmail.com>
2022-01-25 22:57:11 +1300

    Started Chapter 15

---
+++ Chapter15/distance_between_points.py
@@ -0,0 +1,21 @@
+import math
+class Point:
+    """Represents a point in 2D space"""
+
+
+def distance_between_points(a, b):
+    try:
+        dx = abs(a.x - b.x)
+        dy = abs(a.y - b.y)
+    except AttributeError:
+        print("distance_between_points requires both arguments to have 'x' and 'y' attributes")
+        return None
+    return math.sqrt(dx**2 + dy**2)
+
+first = Point()
+second = Point()
+first.x = 0.0
+first.y = 0.0
+second.x = 3.0
+second.y = 4.0
+print(distance_between_points(first, second))
