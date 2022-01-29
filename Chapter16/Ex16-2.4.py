05dfc5693ced5e53f8e0512b25d6253a5fb1c9ee
Mark <cookson.mark@gmail.com>
2022-01-30 12:18:02 +1300

    Finished Chapter 16

--- Chapter16/Ex16-2.4.py
+++ Chapter16/Ex16-2.4.py
@@ -1,20 +1,21 @@
 import datetime
 
-def find_double_day(older_birthday, younger_birthday):
+def find_n_day(older_birthday, younger_birthday, n):
     """Takes 2 datetime.date objects representing different days, returns the date when one is
     twice as old as the other
     Precondition: older_birthday < younger_birthday"""
     assert older_birthday < younger_birthday
     difference = younger_birthday - older_birthday
-    double_day = younger_birthday + difference
-    return double_day
+    n_day = younger_birthday + (difference * 1/(n-1))
+    return n_day
 
-birthday1 = datetime.date(1975, 2, 1)
-birthday2 = datetime.date(1976, 1, 1)
+birthday1 = datetime.date(1975, 1, 1)
+birthday2 = datetime.date(1975, 2, 4)
 
 print('Birthday 1 is', birthday1)
 print('Birthday 2 is', birthday2)
-print('Double day for these two birthdays is', find_double_day(birthday1, birthday2))
+n = 4
+print(n, 'day for these two birthdays is', find_n_day(birthday1, birthday2, n))
 
 
 
